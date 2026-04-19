bd80c2e37d9133ff0829311c21d1271733464489
8uff3r <8uff3r@gmail.com>
2026-04-20 00:25:07 +0330

    models: Add ColorHex to some models, improve Room model

--- internal/models/models.go
+++ internal/models/models.go
@@ -53,10 +53,10 @@ type Hotel struct {
 
 type Room struct {
 	Base
-	HotelID     *uint     `json:"hotelId"`
+	HotelID     *uint     `json:"hotelId,omitempty"`
 	Name        string    `json:"name"`
-	RoomNumber  string    `gorm:"not null" json:"roomNumber"`
-	Floor       *int      `json:"floor"`
+	RoomNumber  string    `gorm:"not null" json:"roomNumber" validate:"required,min=1"`
+	Floor       int       `json:"floor"`
 	Capacity    int       `gorm:"not null;default:2" json:"capacity"`
 	BasePrice   float64   `gorm:"not null;default:0" json:"basePrice"`
 	Amenities   []Amenity `gorm:"many2many:room_amenities;" json:"amenities" translate:"true"`
@@ -74,11 +74,13 @@ type Room struct {
 type RoomType struct {
 	Base
 	TranslateBase
+	ColorHex string `gorm:"type:char(6);default:null" json:"colorHex,omitempty"`
 }
 
 type RoomStatus struct {
 	Base
 	TranslateBase
+	ColorHex string `gorm:"type:char(6);default:null" json:"colorHex,omitempty"`
 }
 
 type Amenity struct {
