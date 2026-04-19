5aa74f88c05c4df24ae1571dadb4b5f43b7b7c3c
8uff3r <8uff3r@gmail.com>
2026-04-19 05:25:31 +0330

    web: Add app.config.ts to configure nuxt-ui

---
+++ web/app/app.config.ts
@@ -0,0 +1,15 @@
+export default defineAppConfig({
+  ui: {
+    input: {
+      slots: {
+        root: "w-full",
+        base: "w-full rounded-md border-0 appearance-none placeholder:text-dimmed focus:outline-none disabled:cursor-not-allowed disabled:opacity-75",
+      },
+    },
+    select: {
+      slots: {
+        base: "w-full",
+      },
+    },
+  },
+});
