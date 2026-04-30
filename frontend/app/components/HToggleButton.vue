75cce9e0b0d8b6bcd46d54b9fef56aa4d514330c
8uff3r <8uff3r@gmail.com>
2026-04-30 17:46:06 +0330

    frontend: add HToggleButton component

---
+++ frontend/app/components/HToggleButton.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const model = defineModel<boolean>({ default: false });
+</script>
+<template>
+  <UButton
+    :variant="model ? 'solid' : 'outline'"
+    :color="model ? 'primary' : 'neutral'"
+    @click="model = !model"
+  >
+    <slot />
+  </UButton>
+</template>
