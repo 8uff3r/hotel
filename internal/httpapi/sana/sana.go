05fe05ea2f8a85f1eb866828c4bc3ca148a84227
8uff3r <8uff3r@gmail.com>
2026-05-02 15:55:43 +0330

    minor fix

--- internal/httpapi/sana/sana.go
+++ internal/httpapi/sana/sana.go
@@ -5,7 +5,6 @@ import (
 	"hotel/internal/models"
 
 	"github.com/go-fuego/fuego"
-	h "github.com/go-fuego/fuego"
 	"gorm.io/gorm"
 )
 
@@ -18,38 +17,38 @@ func New(db *gorm.DB) SanaModule {
 }
 
 func (m SanaModule) RegisterRoutes(api *httpapi.API, s *fuego.Server) {
-	h.Get(s, "/travel-reasons", m.getTravelReasons)
-	h.Get(s, "/family-relationships", m.getFamilyRelationships)
-	h.Get(s, "/nationalities", m.getNationalities)
-	h.Get(s, "/countries", m.getCountries)
-	h.Get(s, "/occupations", m.getOccupations)
+	fuego.Get(s, "/travel-reasons", m.getTravelReasons)
+	fuego.Get(s, "/family-relationships", m.getFamilyRelationships)
+	fuego.Get(s, "/nationalities", m.getNationalities)
+	fuego.Get(s, "/countries", m.getCountries)
+	fuego.Get(s, "/occupations", m.getOccupations)
 }
 
-func (m SanaModule) getTravelReasons(c h.ContextNoBody) ([]models.TravelReason, error) {
+func (m SanaModule) getTravelReasons(c fuego.ContextNoBody) ([]models.TravelReason, error) {
 	var reasons []models.TravelReason
 	err := m.db.Find(&reasons).Error
 	return reasons, err
 }
 
-func (m SanaModule) getFamilyRelationships(c h.ContextNoBody) ([]models.FamilyRelationship, error) {
+func (m SanaModule) getFamilyRelationships(c fuego.ContextNoBody) ([]models.FamilyRelationship, error) {
 	var relationships []models.FamilyRelationship
 	err := m.db.Find(&relationships).Error
 	return relationships, err
 }
 
-func (m SanaModule) getNationalities(c h.ContextNoBody) ([]models.Nationality, error) {
+func (m SanaModule) getNationalities(c fuego.ContextNoBody) ([]models.Nationality, error) {
 	var nationalities []models.Nationality
 	err := m.db.Find(&nationalities).Error
 	return nationalities, err
 }
 
-func (m SanaModule) getCountries(c h.ContextNoBody) ([]models.Country, error) {
+func (m SanaModule) getCountries(c fuego.ContextNoBody) ([]models.Country, error) {
 	var countries []models.Country
 	err := m.db.Find(&countries).Error
 	return countries, err
 }
 
-func (m SanaModule) getOccupations(c h.ContextNoBody) ([]models.Occupation, error) {
+func (m SanaModule) getOccupations(c fuego.ContextNoBody) ([]models.Occupation, error) {
 	var occupations []models.Occupation
 	err := m.db.Find(&occupations).Error
 	return occupations, err
